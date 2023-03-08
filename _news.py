94e39b173a64990f7103e415dc48c36989bd9da8
Elias <eliasantoniorodrigues1@gmail.com>
2023-03-07 22:14:52 -0300

    finished news scraping

--- _news.py
+++ _news.py
@@ -1,39 +1,43 @@
 import requests
-from _settings import ibov
+from _settings import ibov, project_name, dataset
+from _utils import create_bigquery_table
 from bs4 import BeautifulSoup
+from datetime import datetime as dt
+import pprint
+import pandas as pd
 
 
-ticker = ibov[0]
-url = f'https://www.google.com/search?q={ticker}&sxsrf=AJOqlzWBR3DgjOJZnL0kU_Gzd1XxsDEYuA:1677803825775&source=lnms&tbm=nws&sa=X&ved=2ahUKEwjl3aaowr79AhWEGbkGHQbDC2kQ_AUoAnoECAEQBA&biw=2400&bih=1184&dpr=0.8'
+for ticker in ibov:
+    try:
+        url = f'https://www.google.com/search?q={ticker}&source=lnms&tbm=nws&sa=X&ved=2ahUKEwi2n6rT-sr9AhUmKbkGHcS7AfMQ_AUoAnoECAEQBA&biw=1920&bih=929&dpr=1'
 
+        r = requests.get(url)
 
-# r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        links = soup.find_all('a', href=True)
+        search = []
+        for link in links:
+            news = link.find('h3')
+            source = link.find('div')
 
-# salva os dados da pesquisa em um arquivo html
-# with open(f'search_{ticker}.html', 'w', enconding='utf-8') as f:
-#    f.write(r.text)
+            if news and link:
+                news = {
+                    'ticker': ticker,
+                    'date': dt.today().strftime('%Y-%m-%d %H:%M:%S'),
+                    'url': link['href'].replace('/url?q=', ''),
+                    'source': source.text.split()[-1],
+                    'new': news.text.strip()
+                }
 
-with open(f'search_{ticker}.html', 'r', encoding='latin-1') as f:
-    html = f.read()
+                search.append(news)
 
-soup = BeautifulSoup(html, 'html.parser')
-h3 = soup.find_all('h3')
-span = soup.find_all('span')
-links = soup.find_all('a', href=True)
-
-i = 0
-for link in links:
-    if ticker in link['href']:
-        print(i, link['href'])
-
-        i += 1
-
-# for title in span:
-#    print(title.text)
-
-
-
-# earch = soup.find(id='search')
-# print(search.text)
-# for result in search.find_all('div'):
-#    print(result.text)
+        # print(f'Total of {len(search)} news founded!')
+        # printer.pprint(search)
+        df = pd.DataFrame(search)
+        print(df.head())
+        create_bigquery_table(df=df, dataset_tablename=f'{project_name}.{dataset}.t_news',
+                            gcp_project_name=project_name)
+    except Exception as e:
+        print(e)
+        print('Continue...')
+        
